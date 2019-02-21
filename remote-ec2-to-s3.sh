7ddce499c2e311c30650d8361dd09c27687892be
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-02-21 21:59:45 +0900

    Fix the sh file name

--- remote-ec2-to-s3.sh
+++ remote-ec2-to-s3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # command to create a file to copy
-# for the purpose of the demo, this gen-text is super simple
-./gen-text.sh
+# for the purpose of the demo, this remote-gen-text.sh is super simple
+./remote-gen-text.sh
 
 # On Amazon Linux, AWS CLI is already installed
 # Note that an instance profiler setup is needed to execute AWS CLI on EC2
